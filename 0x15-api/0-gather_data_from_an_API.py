bd90da0e1daeb54da4cc01f8d2b0dfd079f159a7
hoodmanb <joshuadebravo@gmail.com>
2024-05-20 19:54:09 +0100

    done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Python script that uses REST API to fetch data.
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    user = get('https://jsonplaceholder.typicode.com/users/' + argv[1])
+    name = user.json().get("name")
+    todo = get('https://jsonplaceholder.typicode.com/todos')
+    todos = todo.json()
+    task_list = []
+    tasks = 0
+    completed = 0
+    for item in todos:
+        if item.get("userId") == int(argv[1]):
+            tasks +=1
+            if item.get("completed") is True:
+                completed +=1
+                task_list.append(item.get("title"))
+    print("Employee {} is done with tasks({}/{})".format(name,completed,tasks))
+
+    for i in task_list:
+        print("\t {}".format(i))
