48ee7fe0a1239544d0aedf0f19cf9b6b4266235d
hoodmanb <joshuadebravo@gmail.com>
2024-06-04 19:56:14 +0100

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -18,7 +18,7 @@ def top_ten(subreddit):
         None
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "chrome 5.8"}
+    headers = {"User-Agent": "custom_user_agent_v1.0"}
     params = {"limit": 10}
     response = requests.get(url, headers=headers, allow_redirects=False,
                             params=params)
@@ -29,3 +29,7 @@ def top_ten(subreddit):
             print(post.get('data', {}).get('title', None))
     else:
         print(None)
+
+
+# Example usage
+# top_ten("python")
