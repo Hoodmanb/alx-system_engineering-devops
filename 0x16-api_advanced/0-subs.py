c37832f6ad999976cce02f70a3be7ff69294c0f9
hoodmanb <joshuadebravo@gmail.com>
2024-06-04 19:34:20 +0100

    added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-""" a script that has a method that returns
 """
+A script that contains a method to return the number of subscribers
+to a given subreddit.
+"""
+
 import requests
 import sys
 
 
 def number_of_subscribers(subreddit):
-    """ a methos that returns the number of a subscribers """
+    """Returns the number of subscribers for a given subreddit."""
     headers = {"User-Agent": "xica369"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        return (response.json().get('data').get('subscribers'))
+        return response.json().get('data').get('subscribers')
     else:
-        return (0)
+        return 0
